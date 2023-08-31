862139aa97316e0a5606885f1beb2160cd1af1aa
ray zee <raymo@PC.ZINGSERVER1.GOV>
2023-08-31 16:07:41 +0200

    path converters

--- challenges/views.py
+++ challenges/views.py
@@ -14,15 +14,42 @@ from django.http import HttpResponse, HttpResponseNotFound
 #     return HttpResponse("this is march")
 
 
+# def monthly_challenge(request, month):
+#     challenge_text = None
+#     if month == "january":
+#         challenge_text = "this is january"
+#     elif month == "february":
+#         challenge_text = "this is february"
+#     elif month == "march":
+#         challenge_text = "this is march"
+#     else:
+#         return HttpResponseNotFound("this month is not available")
+#     return HttpResponse(challenge_text)
+
+monthly_challenges = {
+    "january": "this is january",
+    "february": "this is february",
+    "march": "this is march",
+    "april": "this is april",
+    "may": "this is may",
+    "june": "this is june",
+    "july": "this is july",
+    "august": "this is august",
+    "september": "this is september",
+    "october": "this is october",
+    "november": "this is november",
+    "december": "this is december"
+}
+
+
 def monthly_challenge(request, month):
-    challenge_text = None
-    if month == "january":
-        challenge_text = "this is january"
-    elif month == "february":
-        challenge_text = "this is february"
-    elif month == "march":
-        challenge_text = "this is march"
-    else:
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
         return HttpResponseNotFound("this month is not available")
-    return HttpResponse(challenge_text)
+
+    
+    
+
 
