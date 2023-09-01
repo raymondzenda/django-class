4f0d0eb2962c357212dde0b158f9662dc5849dc9
ray zee <raymo@PC.ZINGSERVER1.GOV>
2023-09-01 11:11:26 +0200

    redirect

--- challenges/views.py
+++ challenges/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 # Create your views here.
 
 # def january(request):
 #     return HttpResponse("This is working very well !!!")
-
 
 # def february(request):
 #     return HttpResponse("this is february")
@@ -40,6 +39,16 @@ monthly_challenges = {
     "november": "this is november",
     "december": "this is december"
 }
+
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+
+    if month > len(months):
+        return HttpResponseNotFound("invalid month")
+    
+    redirect_month = months[month-1]
+    return HttpResponseRedirect("/challenges/" + redirect_month)
 
 
 def monthly_challenge(request, month):
